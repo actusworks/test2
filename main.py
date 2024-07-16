4b62082dbb81e5f53c4a743490049c532fac2465
actus01 <actusanima@gmail.com>
2024-07-16 06:26:11 +0300

    Deploying the program.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World 2!"
 
 if __name__ == "__main__":
     print(hello_world())
--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from main import hello_world
 
 class TestMain(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(hello_world(), "Hello, World!")
+        self.assertEqual(hello_world(), "Hello, World 2!")
 
 if __name__ == "__main__":
     unittest.main()
